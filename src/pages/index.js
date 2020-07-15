ce97ecd96105d6eb721882ed29aaf52d9ff6fde2
Abhishek Katyare <abhishekkatyare@gmail.com>
2020-07-15 17:38:30 +0530

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "Abhishek Katyare"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -22,14 +22,15 @@ class IndexPage extends React.Component {
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
+        <p>Seeker of truth, who is on the path to becoming a mystic.</p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
+        </Link>
+        <Link to="/about/">
+          <Button marginTop="35px">About Me</Button>
+        </Link>
+        <Link to="/contact/">
+          <Button marginTop="35px">Contact Me</Button>
         </Link>
       </Layout>
     )
